98024c6f59137cf6cc1319811c00e52e3b0f0eb9
xfxpositions <xfxpositions@gmail.com>
2023-09-29 19:49:45 +0300

    user/delete update findone findbyid routes added

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -21,7 +21,7 @@ const userRoutes = new Elysia({ prefix: "/user" })
         password: t.String(),
       }),
       detail: {
-        tags: ["User", "Auth"],
+        tags: ["Auth"],
       },
     }
   )
@@ -54,7 +54,218 @@ const userRoutes = new Elysia({ prefix: "/user" })
         email: t.String(),
       }),
       detail: {
-        tags: ["User", "Auth"],
+        tags: ["Auth"],
+      },
+    }
+  )
+  .delete(
+    "/delete/:id",
+    ({ params }) => {
+      if (!params.id) {
+        return new Response(
+          JSON.stringify({
+            status: "bad request",
+            detail: "id is required -> /delete/:id",
+          }),
+          { status: 400 }
+        );
+      }
+
+      return User.findByIdAndDelete(params.id)
+        .then((deletedUser) => {
+          if (!deletedUser) {
+            return new Response(
+              JSON.stringify({
+                status: "not found",
+                detail: `User with ID ${params.id} not found.`,
+              }),
+              { status: 404 }
+            );
+          }
+
+          return new Response(
+            JSON.stringify({
+              status: "success",
+              detail: `User ${params.id} deleted.`,
+            }),
+            { status: 200 }
+          );
+        })
+        .catch((error) => {
+          return new Response(
+            JSON.stringify({
+              status: "error",
+              detail: error.message, // Use error.message directly
+            }),
+            { status: 500 }
+          );
+        });
+    },
+    {
+      detail: {
+        tags: ["User"],
+      },
+    }
+  )
+  .put(
+    "/update/:id",
+    ({ params, body }) => {
+      if (!params.id) {
+        return new Response(
+          JSON.stringify({
+            status: "bad request",
+            detail: "id is required -> /update/:id",
+          }),
+          { status: 400 }
+        );
+      }
+
+      const updatedData = body as Record<string, any>; // Define the type of the body
+
+      return User.findByIdAndUpdate(params.id, updatedData, { new: true })
+        .then((updatedUser: any) => {
+          // Specify the type for updatedUser
+          if (!updatedUser) {
+            return new Response(
+              JSON.stringify({
+                status: "not found",
+                detail: `User with ID ${params.id} not found.`,
+              }),
+              { status: 404 }
+            );
+          }
+
+          return new Response(
+            JSON.stringify({
+              status: "success",
+              detail: `User ${params.id} updated.`,
+              result: updatedUser,
+            }),
+            { status: 200 }
+          );
+        })
+        .catch((error) => {
+          return new Response(
+            JSON.stringify({
+              status: "error",
+              detail: error.message,
+            }),
+            { status: 500 }
+          );
+        });
+    },
+    {
+      detail: {
+        tags: ["User"],
+      },
+    }
+  )
+  .get(
+    "/findbyid/:id",
+    ({ params }) => {
+      if (!params.id) {
+        return new Response(
+          JSON.stringify({
+            status: "bad request",
+            detail: "id is required -> /findbyid/:id",
+          }),
+          { status: 400 }
+        );
+      }
+
+      return User.findById(params.id)
+        .then((user) => {
+          if (!user) {
+            return new Response(
+              JSON.stringify({
+                status: "not found",
+                detail: `User with ID ${params.id} not found.`,
+              }),
+              { status: 404 }
+            );
+          }
+
+          // delete password and passwordSaltKey
+          user.password = "";
+          user.passwordSalt = "";
+
+          return new Response(
+            JSON.stringify({
+              status: "success",
+              detail: `User found with ID ${params.id}`,
+              result: user,
+            }),
+            { status: 200 }
+          );
+        })
+        .catch((error) => {
+          return new Response(
+            JSON.stringify({
+              status: "error",
+              detail: error.message,
+            }),
+            { status: 500 }
+          );
+        });
+    },
+    {
+      detail: {
+        tags: ["User"],
+      },
+    }
+  )
+  .get(
+    "/findone",
+    ({ query }) => {
+      const filter: { [key: string]: any } = {};
+
+      const allowedFields = ["email", "userId", "name", "username", "phone"];
+
+      // Iterate over the allowed fields and add them to the filter if they exist in the query
+      allowedFields.forEach((field) => {
+        if (query[field]) {
+          filter[field] = query[field];
+        }
+      });
+
+      return User.findOne(filter)
+        .then((user) => {
+          if (!user) {
+            return new Response(
+              JSON.stringify({
+                status: "not found",
+                detail: "User not found with the provided criteria.",
+              }),
+              { status: 404 }
+            );
+          }
+
+          // delete password and passwordSaltKey
+          user.password = "";
+          user.passwordSalt = "";
+
+          return new Response(
+            JSON.stringify({
+              status: "success",
+              detail: "User found.",
+              result: user,
+            }),
+            { status: 200 }
+          );
+        })
+        .catch((error) => {
+          return new Response(
+            JSON.stringify({
+              status: "error",
+              detail: error.message,
+            }),
+            { status: 500 }
+          );
+        });
+    },
+    {
+      detail: {
+        tags: ["User"],
       },
     }
   );
